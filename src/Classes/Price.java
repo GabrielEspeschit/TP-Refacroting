feedc8bb976fd655a94b4811207abdfd80d4e407
Gabriel Espeschit <gabrielespeschit@gmail.com>
2019-11-10 15:38:57 -0300

    Replace Conditional with Polymorphism

---
+++ src/Classes/Price.java
@@ -0,0 +1,10 @@
+package Classes;
+
+abstract class Price {
+    abstract int getPriceCode();
+    abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
+}
